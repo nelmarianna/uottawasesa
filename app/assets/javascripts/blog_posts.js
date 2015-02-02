eff48c6a517068cb1396921e513bc7142c20f7ce
Craig <craigbryan.csb@gmail.com>
2015-02-02 15:23:28 -0500

    Fixed editor attempting to load on pages that don't need it.

--- app/assets/javascripts/blog_posts.js
+++ app/assets/javascripts/blog_posts.js
@@ -4,26 +4,29 @@
 // For the edit and new blog posts editor
 
 $(document).ready(function() {
-  var editor_opts = {
-    autogrow: true,
-    file: {name: "local_blog"}
-  };
+  
+  if($(".editor-box").length !== 0) {
+    var editor_opts = {
+      autogrow: true,
+      file: {name: "local_blog"}
+    };
 
-  var editor = new EpicEditor(editor_opts).load();
+    var editor = new EpicEditor(editor_opts).load();
 
-  //Put any custom editor styles here
-  $(editor.getElement('editor').body).css({
-    'background': 'none'
-  });
+    //Put any custom editor styles here
+    $(editor.getElement('editor').body).css({
+      'background': 'none'
+    });
 
-  //Sends the local file to the server as the blog body
-  $("#publish_button, #save_button").click(function(e) {
-    $("#body_field").val(editor.exportFile());
-  });
+    //Sends the local file to the server as the blog body
+    $("#publish_button, #save_button").click(function(e) {
+      $("#body_field").val(editor.exportFile());
+    });
 
-  //clears the local file
-  $("#discard_button").click(function(e) {
-    editor.remove('local_blog');
-    editor.open('local_blog');
-  });
+    //clears the local file
+    $("#discard_button").click(function(e) {
+      editor.remove('local_blog');
+      editor.open('local_blog');
+    });
+  }
 });
